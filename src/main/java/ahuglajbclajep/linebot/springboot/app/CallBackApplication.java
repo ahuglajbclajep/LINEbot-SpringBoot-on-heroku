8815f6a5011cb5fe2d1ee0492e7ec8819aa6c62c
ahuglajbclajep <ahuglajbclajep@gmail.com>
2017-02-18 10:34:46 +0900

    [upd]send multiple replies

--- src/main/java/ahuglajbclajep/linebot/springboot/app/CallBackApplication.java
+++ src/main/java/ahuglajbclajep/linebot/springboot/app/CallBackApplication.java
@@ -1,14 +1,26 @@
 package ahuglajbclajep.linebot.springboot.app;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import com.linecorp.bot.client.LineMessagingServiceBuilder;
+import com.linecorp.bot.model.ReplyMessage;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.FollowEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.event.message.ImageMessageContent;
+import com.linecorp.bot.model.event.message.StickerMessageContent;
+import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 @LineMessageHandler
@@ -18,13 +30,56 @@ public class CallBackApplication {
 	}
 
 	@EventMapping
-	public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+	public void handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
 		System.out.println("event: " + event);
-		return new TextMessage(event.getMessage().getText());
+		List<Message> reply = new ArrayList<>();
+
+		String[] spl = event.getMessage().getText().split(" ", 2);
+		if ("@wol".equals(spl[0])) {
+			reply.add(new TextMessage("はい、睦月が用意するね！"));
+			try{
+				String url = URLEncoder.encode(spl[1], "UTF-8");
+				reply.add(new TextMessage("http://www.wolframalpha.com/input/?i=" + url));
+			}catch (ArrayIndexOutOfBoundsException | UnsupportedEncodingException e){
+				reply.add(new TextMessage("http://www.wolframalpha.com"));
+			}
+
+		} else {
+			reply.add(new TextMessage("にゃしぃ"));
+		}
+
+		execute(new ReplyMessage(event.getReplyToken(), reply));
 	}
 
 	@EventMapping
-	public void handleDefaultMessageEvent(Event event) {
+	public TextMessage handleImageMessageEvent(MessageEvent<ImageMessageContent> event) {
 		System.out.println("event: " + event);
+		return new TextMessage("睦月、負ける気がしないのね！");
+	}
+
+	@EventMapping
+	public TextMessage handleStickerMessageEvent(MessageEvent<StickerMessageContent> event) {
+		System.out.println("event: " + event);
+		return new TextMessage("なんですかなんですかぁー？");
+	}
+
+	@EventMapping
+	public TextMessage handleFollowEvent(FollowEvent event) {
+		System.out.println("event: " + event);
+		return new TextMessage("睦月、砲雷撃戦始めるよ♪");
+	}
+
+	@EventMapping
+	public TextMessage handleDefaultMessageEvent(Event event) {
+		System.out.println("event: " + event);
+		return new TextMessage("睦月、呼んだ？");
+	}
+
+	private void execute(ReplyMessage replyMessage) {
+		try {
+			LineMessagingServiceBuilder.create(System.getenv("LINE_BOT_CHANNEL_TOKEN")).build().replyMessage(replyMessage).execute();
+		} catch (IOException e) {
+			// void
+		}
 	}
 }
