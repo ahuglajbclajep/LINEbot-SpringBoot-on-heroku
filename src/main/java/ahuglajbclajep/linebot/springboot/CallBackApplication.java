5fc574c96e704f06af38143cd1df7dc43bdb02ef
ahuglajbclajep <ahuglajbclajep@gmail.com>
2017-02-26 01:51:00 +0900

    [upd]add '@qr' command

--- src/main/java/ahuglajbclajep/linebot/springboot/CallBackApplication.java
+++ src/main/java/ahuglajbclajep/linebot/springboot/CallBackApplication.java
@@ -1,25 +1,35 @@
 package ahuglajbclajep.linebot.springboot;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 import com.linecorp.bot.client.LineMessagingServiceBuilder;
 import com.linecorp.bot.model.ReplyMessage;
 import com.linecorp.bot.model.event.Event;
-import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.FollowEvent;
-import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.ImageMessageContent;
 import com.linecorp.bot.model.event.message.StickerMessageContent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.ImageMessage;
 import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
 
 @SpringBootApplication
@@ -29,17 +39,38 @@ public class CallBackApplication {
 		SpringApplication.run(CallBackApplication.class, args);
 	}
 
+
 	@EventMapping
 	public void handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
 		System.out.println("event: " + event);
 		List<Message> reply = new ArrayList<>();
+		String[] args = event.getMessage().getText().split(" ", 2);
 
-		String[] spl = event.getMessage().getText().split(" ", 2);
-		if ("@wol".equals(spl[0])) {
+		if ("@qr".equals(args[0])) {
+			reply.add(new TextMessage("えへへ、どうぞです♪"));
+
+			try {
+				String qr = createQR(args[1], event.getMessage().getId());
+
+				StringBuffer urlbuff = new StringBuffer("https://")
+						.append(System.getenv("APP_NAME"))
+						.append(".herokuapp.com")
+						.append(qr);
+				String url = urlbuff.toString();
+
+				reply.add(new ImageMessage(url, url));
+
+			} catch (ArrayIndexOutOfBoundsException | IOException | WriterException e) {
+				reply.add(new TextMessage("およ？およよ？"));
+			}
+
+		} else if ("@wol".equals(args[0])) {
 			reply.add(new TextMessage("はい、睦月が用意するね！"));
+
 			try{
-				String url = URLEncoder.encode(spl[1], "UTF-8");
+				String url = URLEncoder.encode(args[1], "UTF-8");
 				reply.add(new TextMessage("http://www.wolframalpha.com/input/?i=" + url));
+
 			}catch (ArrayIndexOutOfBoundsException | UnsupportedEncodingException e){
 				reply.add(new TextMessage("http://www.wolframalpha.com"));
 			}
@@ -51,11 +82,13 @@ public class CallBackApplication {
 		execute(new ReplyMessage(event.getReplyToken(), reply));
 	}
 
+
 	@EventMapping
 	public TextMessage handleImageMessageEvent(MessageEvent<ImageMessageContent> event) {
 		System.out.println("event: " + event);
 		return new TextMessage("睦月、負ける気がしないのね！");
 	}
+
 
 	@EventMapping
 	public TextMessage handleStickerMessageEvent(MessageEvent<StickerMessageContent> event) {
@@ -63,11 +96,13 @@ public class CallBackApplication {
 		return new TextMessage("なんですかなんですかぁー？");
 	}
 
+
 	@EventMapping
 	public TextMessage handleFollowEvent(FollowEvent event) {
 		System.out.println("event: " + event);
 		return new TextMessage("睦月、砲雷撃戦始めるよ♪");
 	}
+
 
 	@EventMapping
 	public TextMessage handleDefaultMessageEvent(Event event) {
@@ -75,9 +110,28 @@ public class CallBackApplication {
 		return new TextMessage("睦月、呼んだ？");
 	}
 
+
 	private void execute(ReplyMessage replyMessage) {
 		try {
 			LineMessagingServiceBuilder.create(System.getenv("LINE_BOT_CHANNEL_TOKEN")).build().replyMessage(replyMessage).execute();
 		} catch (IOException e) {}
+	}
+
+
+	private String createQR(String arg, String id) throws WriterException, IOException {
+		EnumMap<EncodeHintType, Object> hints = new EnumMap<>(EncodeHintType.class);
+
+		hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+		BitMatrix bitMatrix = new QRCodeWriter().encode(arg, BarcodeFormat.QR_CODE, 185, 185, hints);
+		// サイズはバージョン毎に4セル刻みで大きくなり,バージョン40で171x171, デフォルトのMARGINは上下左右4セル
+
+		BufferedImage image = MatrixToImageWriter.toBufferedImage(bitMatrix);
+
+		StringBuffer fileName = new StringBuffer("/tmp/");
+		fileName.append(id);
+		fileName.append(".jpg");
+		ImageIO.write(image, "JPEG", new File(fileName.toString()));  // tmpフォルダ以下に出力
+
+		return fileName.toString();
 	}
 }
--- src/main/java/ahuglajbclajep/linebot/springboot/SendPicture.java
+++ src/main/java/ahuglajbclajep/linebot/springboot/SendPicture.java
@@ -1,16 +1,35 @@
 package ahuglajbclajep.linebot.springboot;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/tmp")
 public class SendPicture {
 
-    @RequestMapping(method = RequestMethod.GET)
-    public void hello() {
-        System.out.println("hello");
+    @RequestMapping(value="/{id}", method=RequestMethod.GET, produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<byte[]> hello(@PathVariable String id) {
+        byte[] image;
+        try {
+            Path path = Paths.get("/tmp",id + ".jpg");
+            System.out.println(path.toString());
+            image = Files.readAllBytes(path);
+
+        } catch (InvalidPathException | IOException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(image);
     }
 }
